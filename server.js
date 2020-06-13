3b10662aa7a9584a9d55c5be979e1ef92d25bb1b
Rayson Kong <kong.rayson@gmail.com>
2020-06-13 10:37:07 +0800

    finish

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 // setInterval(() => {console.log("1 Second interval")}, 1000);
-
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    //console.log(req);
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.write('<h1>Hello World!</h1>')
-    res.end();
+    if (req.url === '/') {
+      res.writeHead(200, {'content-type': 'text/html'});
+      const homePageHTML = fs.readFileSync('./node.html');
+      res.write(homePageHTML);
+      res.end();
+    } else if (req.url === "/logo.svg") {
+        res.writeHead(200, {'content-type': 'image/svg+xml'});
+        const image = fs.readFileSync('./logo.svg');
+        res.write(image);
+        res.end(); 
+    } else if (req.url === '/style.css') {
+        res.writeHead(200, {'content-type': 'text/css'});
+        const css = fs.readFileSync('./style.css');
+        res.write(css);
+        res.end();
+    } else {
+        res.writeHead(404, {'content-type': 'text/html'});
+        res.write('<h4>Sorry, this page is not available</h4>');
+    }
 })
 
 server.listen(3000);
